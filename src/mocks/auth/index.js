77767b6ca93fce607c3af688683011c7a8bb7563
definite2 <edefne878@gmail.com>
2021-10-02 20:11:57 +0300

    return mock error response

--- src/mocks/auth/index.js
+++ src/mocks/auth/index.js
@@ -1,40 +1,8 @@
-import { LOGIN_URL, GET_USER_BY_ACCESSTOKEN_URL } from "../../services/authApi";
-import { mockUserList } from "./mockUsersList";
+import { LOGIN_URL } from "../../services/authApi";
+
 export function mockAuth(mock) {
   mock.onPost(LOGIN_URL).reply(({ data }) => {
-    const { username, password } = JSON.parse(data);
-    if (username && password) {
-      const user = mockUserList.find(
-        (x) =>
-          x.username.toLowerCase() === username.toLowerCase() &&
-          x.password === password
-      );
-
-      if (user) {
-        const auth = user.auth;
-        return [200, { ...auth, password: undefined }];
-      }
-    }
-
-    return [400];
+    const { username } = JSON.parse(data);
+    return [403, { message: `User '${username}' not found` }];
   });
-
-  mock
-    .onGet(GET_USER_BY_ACCESSTOKEN_URL)
-    .reply(({ headers: { Authorization } }) => {
-      const accessToken =
-        Authorization &&
-        Authorization.startsWith("Bearer ") &&
-        Authorization.slice("Bearer ".length);
-      if (accessToken) {
-        const user = mockUserList.find(
-          (x) => x.auth?.accessToken === accessToken
-        );
-
-        if (user) {
-          return [200, { ...user, password: undefined }];
-        }
-      }
-      return [401];
-    });
 }
