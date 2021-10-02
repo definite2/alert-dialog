5d6f49839b9e6bd18ef71e5e9e276e20237b2875
definite2 <edefne878@gmail.com>
2021-10-02 20:11:28 +0300

    add login and error portal to app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect } from "react";
+import Login from "./components/Login";
+import { ErrorPortal } from "./components/Alert";
+import { useDispatch } from "react-redux";
+import { removeErrors } from "./store/error/actions";
 function App() {
+  const dispatch = useDispatch();
+  //clear errors on page mounted
+  useEffect(() => {
+    dispatch(removeErrors());
+  }, [dispatch]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Login />
+      <ErrorPortal />
+    </>
   );
 }
 
