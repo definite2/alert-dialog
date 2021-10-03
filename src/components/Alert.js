4e710c97b1614d09394ad267dfc94aad2ea7091b
definite2 <edefne878@gmail.com>
2021-10-03 09:49:39 +0300

    add prptypes to alert component

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Header, Segment, TransitionablePortal } from "semantic-ui-react";
+import { Header, Segment, TransitionablePortal } from "semantic-ui-react"; //any UI library or you can create your own popup component
 import { useSelector, useDispatch } from "react-redux";
+import PropTypes from "prop-types";
 import { removeErrors } from "../store/error/actions";
 export const ErrorPortal = () => {
   const dispatch = useDispatch();
@@ -12,17 +13,29 @@ export const ErrorPortal = () => {
   return (
     <TransitionablePortal
       open={openDialog}
-      transition={{ animation: "browse right",duration:900 }}
+      transition={{ animation: "browse right", duration: 900 }}
       onClose={handleClose}
       openOnTriggerClick
     >
       <Segment
         color="red"
-        style={{ left: "40%", position: "fixed", top: "10%", zIndex: 1000, width:"400px", height:"120px"}}
+        style={{
+          left: "40%",
+          position: "fixed",
+          top: "10%",
+          zIndex: 1000,
+          width: "400px",
+          height: "120px",
+        }}
       >
         <Header>{title}</Header>
         <p>{message}</p>
       </Segment>
     </TransitionablePortal>
   );
+};
+ErrorPortal.propTypes = {
+  openDialog: PropTypes.bool,
+  title: PropTypes.string,
+  message: PropTypes.string,
 };
