2bce3a3920b10c09c727f5cd6fd6cf83d0b5b1a8
definite2 <edefne878@gmail.com>
2021-10-03 09:49:47 +0300

    delete unused

--- src/mocks/auth/index.js
+++
@@ -1,8 +0,0 @@
-import { LOGIN_URL } from "../../services/authApi";
-
-export function mockAuth(mock) {
-  mock.onPost(LOGIN_URL).reply(({ data }) => {
-    const { username } = JSON.parse(data);
-    return [403, { message: `User '${username}' not found` }];
-  });
-}
